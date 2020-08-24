f86d721d380dd875db9f73eab86c83d87e277f38
hauckc21 <hauck.chris@gmail.com>
2020-08-23 21:02:13 -0500

    app issues

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ station = base.classes.station
 #Flask App Setup
 app = Flask(__name__)
 
-#Flask routes
+# #Flask routes
 @app.route("/")
 def welcome():
     """List all available api routes."""
@@ -40,7 +40,7 @@ def welcome():
 
 # /api/v1.0/precipitation
 # Convert the query results to a dictionary using date as the key and prcp as the value.
-# Return the JSON representation of your dictionary.
+# # Return the JSON representation of your dictionary.
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
@@ -62,15 +62,48 @@ def precipitation():
 
 # /api/v1.0/stations
 # Return a JSON list of stations from the dataset.
+@app.route("/api/v1.0/stations")
+def stations():
+    session = Session(engine)
 
+    results = session.query(station.station, station.name).all()
 
+    session.close()
+
+    #query list of stations
+    station_data = []
+    for station, name in results:
+        station_dict = {}
+        station_dict["station ID"] = station
+        station_dict["station name"] = name
+        station_data.append(station_data)
+    return jsonify(station_data)
 
 # /api/v1.0/tobs
 # Query the dates and temperature observations of the most active station for the last year of data.
 # Return a JSON list of temperature observations (TOBS) for the previous year.
+@app.route("/api/v1.0/tobs")
+def tobs():
+    session = Session(engine)
 
+    #date query
+    date_query = session.query(measurement.date).order_by(measurement.date.desc()).first()
+    last_date = datetime.strptime(latest_date_query[0], '%Y-%m-%d').date()
+    one_year = last_date - relativedelta(months= 12)
 
+    #temperature query
+    results = session.query(measurement.date, measurement.tobs).filter(measurement.date >= year_ago).all()
 
+    session.close()
+
+    temp_data = []
+    for date, tobs in results:
+        temp_dict = {}
+        temp_dict["Date"] = date
+        temp_dict["Temperature"] = tobs
+        temp_date.append(temp_dict)
+
+    return jsonify(temp_data)
 
 # /api/v1.0/<start> and /api/v1.0/<start>/<end>
 # Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
@@ -79,7 +112,8 @@ def precipitation():
 
 
 
-
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
 
