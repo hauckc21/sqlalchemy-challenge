ec25344a561cefacb4e10804473eabc95a4be489
hauckc21 <hauck.chris@gmail.com>
2020-08-24 20:11:15 -0500

    flask app

--- app.py
+++ app.py
@@ -34,8 +34,8 @@ def welcome():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api.v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<start>/<end>"
+        f"/api/v1.0/start<br/>"
+        f"/api/v1.0/start/end"
     )
 
 # /api/v1.0/precipitation
@@ -50,14 +50,14 @@ def precipitation():
 
     session.close()
 
-    precipitation_data = []
+    precip_data = []
     for date, prcp in results:
-        precipitation_dict = {}
-        precipitation_dict["Date"] = date
-        precipitation_dict["Precipitation"] = prcp
-        precipitation_data.append(precipitation_dict)
+        precip_dict = {}
+        precip_dict["Date"] = date
+        precip_dict["Precipitation"] = prcp
+        precip_data.append(precip_dict)
 
-    return jsonify(precipitation_data)
+    return jsonify(precip_data)
 
 
 # /api/v1.0/stations
@@ -78,6 +78,7 @@ def stations():
         station_dict["station name"] = name
         station_data.append(station_data)
     return jsonify(station_data)
+
 
 # /api/v1.0/tobs
 # Query the dates and temperature observations of the most active station for the last year of data.
@@ -109,8 +110,57 @@ def tobs():
 # Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
 # When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date.
 # When given the start and the end date, calculate the TMIN, TAVG, and TMAX for dates between the start and end date inclusive.
+@app.route("/api/v1.0/start")
+def start_date(start):
+    # Create session from Python to the DB
+    session = Session(engine)
+    start_date = datetime.strptime(start, '%Y-%m-%d').date()
 
+    temp_stats = session.query(func.min(measurement.tobs), func.max(measurement.tobs), func.avg(measurement.tobs)).filter(measurement.date >= start_date).all()
+    temps_stats_results = list(np.ravel(temp_stats))
+    
+    min_temp = temps_stats_results[0]
+    max_temp = temps_stats_results[1]
+    avg_temp = temps_stats_results[2]
+    
+    temp_stats_data= []
+    temp_stats_dict = [{"Start Date": start},
+                       {"Minimum Temperature": min_temp},
+                       {"Maximum Temperature": max_temp},
+                       {"Average Temperature": avg_temp}]
 
+    temp_stats_data.append(temp_stats_dict)
+    
+    return jsonify(temp_stats_data)
+
+#-------------------------------------------------------------------
+
+@app.route("/api/v1.0/start/end")
+def start_end_date(start, end):
+    session = Session(engine)
+
+    start_date = datetime.strptime(start, '%Y-%m-%d').date()
+    end_date = datetime.strptime(end, '%Y-%m-%d').date()
+
+    temp_query = session.query(func.min(measurement.tobs), func.max(measurement.tobs), func.avg(measurement.tobs)).\
+        filter(measurement.date >= start_date).filter(measurement.date <= end_date).all()
+    temps_data_results = list(np.ravel(temp_query))
+    
+    min_temp = temps_data_results[0]
+    max_temp = temps_data_results[1]
+    avg_temp = temps_data_results[2]
+    
+    temp_query_data = []
+    temp_data_dict = [{"Start Date": start_date},
+                       {"End Date": end_date},
+                       {"Minimum Temperature": min_temp},
+                       {"Maximum Temperature": max_temp},
+                       {"Average Temperature": avg_temp}]
+
+    temp_stats_data.append(temp_data_dict)
+    
+    return jsonify(temp_query_data)
+#------------------------------------------------------------------------
 
 if __name__ == "__main__":
     app.run(debug=True)
